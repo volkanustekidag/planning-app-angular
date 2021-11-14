5ca4e150d25b75e4cf3d7bacbb0489e4214b0764
volkanustekidag <volkanustekidg@gmail.com>
2021-11-14 22:06:43 +0300

    firebase auth and route

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
   
-  constructor(private dataStorageService: DataStorageService){}
+  isAuthent = false;
+  private userSub: Subscription;
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService){}
+
+  ngOnInit(){
+    console.log("aaaaaaaa");
+    
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAuthent = !!user;
+      console.log(!user);
+      console.log(!!user);
+    });
+  }
 
   onSaveData(){
     this.dataStorageService.storeRecipes();
@@ -16,4 +31,13 @@ export class HeaderComponent {
   onFetchData(){
     this.dataStorageService.fetchRecipes().subscribe();
   }
+
+  onLogout(){
+    this.authService.logout();
+  }
+
+  ngOnDestroy(){
+   this.userSub.unsubscribe(); 
+  }
+
 }
